540484fd323b02fa2bf9cc1a188b950cc4b45bd0
John Nastos <jn@Johns-MBP.home>
2017-06-05 21:17:07 -0700

    Wrote a probably non-working version of getBeatOfTimestamp

--- src/com/practicingmusician/Metronome.kt
+++ src/com/practicingmusician/Metronome.kt
@@ -16,6 +16,8 @@ class Metronome : TimeKeeperSteppable {
     var tempo : Double = 120.0
     var currentBeat : Int = 0
 
+    val beatTimes = mutableListOf<Double>()
+
     // Timestamp of the last beat that happened
     var lastBeatOccuredAt = -1.0
 
@@ -39,12 +41,15 @@ class Metronome : TimeKeeperSteppable {
             lastBeatOccuredAt = timestamp - beatSize
         }
 
+        //if the last beat hasn't even occured yet, don't bother calculating the next one
         if (lastBeatOccuredAt > timestamp) {
             return
         }
 
+        //calculate when the new beat will happen
         val newTime = lastBeatOccuredAt + beatSize
 
+        //how long from now will it happen
         val difference = newTime - timestamp
 
         audioManager.playAudio(audioKey,difference.toInt())
@@ -52,5 +57,56 @@ class Metronome : TimeKeeperSteppable {
         println("Going to play at " + newTime.toInt())
 
         lastBeatOccuredAt = newTime
+
+        beatTimes.add(newTime)
+        currentBeat++
+
     }
+
+    /*
+     * Given a certain timestamp, figure out which beat it belongs to
+     */
+    fun getBeatOfTimestamp(timestamp : Double) : Double {
+
+        var firstItem = -1.0
+        var secondItem = -1.0
+
+
+        var lastItemBefore = -1
+
+        for(index in beatTimes.indices) {
+            val beat = beatTimes[index]
+
+            if (beat > timestamp) {
+                lastItemBefore = index - 1
+
+                if (lastItemBefore == -1) {
+                    //the timestamp is before the first beat
+                    //TODO: This may not be right -- may have to make an artificial next beat for it
+                    return -1.0
+                }
+
+                firstItem = beatTimes[lastItemBefore]
+                secondItem = beat
+
+                break
+            }
+        }
+
+        if (firstItem == null) {
+            return -1.0
+        }
+
+        val distanceBetweenBeats = secondItem - firstItem
+
+        val distanceFromStampToFirstBeat = timestamp - firstItem
+
+        val percentageThroughBeat = distanceFromStampToFirstBeat / distanceBetweenBeats
+
+        //now we need to figure out the index of the beat and add it
+
+        TODO("This isn't the correct return value yet")
+        return percentageThroughBeat
+    }
+
 }
