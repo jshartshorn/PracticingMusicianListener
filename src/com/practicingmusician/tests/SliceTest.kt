9bd8937e09781536b80c5b66b70474f8ea9ef371
John Nastos <jn@Johns-MacBook-Pro.local>
2017-06-07 11:21:29 -0700

    Generating two arrays of samples

--- src/com/practicingmusician/tests/SliceTest.kt
+++ src/com/practicingmusician/tests/SliceTest.kt
@@ -2,6 +2,8 @@ package com.practicingmusician.tests
 
 import com.practicingmusician.models.Slice
 import com.practicingmusician.notes.Note
+import org.w3c.dom.HTMLElement
+import kotlin.browser.document
 
 /**
  * Created by jn on 6/7/17.
@@ -14,6 +16,46 @@ object SliceTest {
     val slicesPerBeat = 32
     val sampleRate = 44100.0
     val bufferLengthInSamples = 1024
+
+
+    fun convertNotesToSamples() : List<Double> {
+        val samples = mutableListOf<Double>()
+        notes.forEach {
+            val numSamplesToCreate = it.duration * secondsPerBeat * sampleRate
+            val freq = it.getFrequency()
+            for (i in 0 until numSamplesToCreate.toInt()) {
+                samples.add(freq)
+            }
+        }
+        return samples
+    }
+
+    fun convertCorrelatedBuffersToSamples() : List<Double> {
+        val lengthOfNotesInSeconds = notes.map { it.duration }.reduce { acc, d -> acc + d } * secondsPerBeat
+        val numCorrelatedBuffers = lengthOfNotesInSeconds * sampleRate / bufferLengthInSamples
+
+        console.log("Num correlated buffers: " + numCorrelatedBuffers)
+
+        val correlatedBuffers = mutableListOf<Double>()
+
+        for (i in 0 until numCorrelatedBuffers.toInt()) {
+            correlatedBuffers.add(440.0) //for now
+        }
+
+        val samplesFromCorrelatedBuffers = mutableListOf<Double>()
+        val samplesPerCorrelatedBuffer = 1024
+
+        correlatedBuffers.forEach {
+            for (i in 0 until samplesPerCorrelatedBuffer) {
+                samplesFromCorrelatedBuffers.add(it)
+            }
+        }
+
+        return samplesFromCorrelatedBuffers
+    }
+
+
+    //Not using these right now....
 
     fun getPremadeSlices() : List<Slice> {
         val slices = mutableListOf<Slice>()
@@ -31,9 +73,25 @@ object SliceTest {
     @JsName("runTest")
     fun runTest() : String {
 
-        val slices = getPremadeSlices()
+        val exerciseSamples = convertNotesToSamples()
 
-        convertTimestampsToSlices()
+        val micSamples = convertCorrelatedBuffersToSamples()
+
+        console.log("Number samples in exercise: " + exerciseSamples.count())
+        console.log("Number samples in mic input: " + micSamples.count())
+
+        val exerciseDiv = document.getElementById("exercise") as HTMLElement
+        exerciseDiv.textContent = "Samples : " + exerciseSamples
+
+        val micDiv = document.getElementById("micInput") as HTMLElement
+        micDiv.textContent = "Samples: " + micSamples
+
+
+//        val slices = getPremadeSlices()
+//
+//        console.log("Number of slices: " + slices.count())
+//
+//        convertTimestampsToSlices()
 
         return "Done"
     }
@@ -41,6 +99,10 @@ object SliceTest {
     fun convertTimestampsToSlices() {
         val lengthOfNotesInSeconds = notes.map { it.duration }.reduce { acc, d -> acc + d } * secondsPerBeat
         val numberTotalSamples = sampleRate * lengthOfNotesInSeconds
+
+        console.log("Length of notes in seconds: " + lengthOfNotesInSeconds)
+
+        console.log("Total slices should be: " + (lengthOfNotesInSeconds / secondsPerBeat * slicesPerBeat))
 
         val sliceLengthInSamples = sampleRate * secondsPerBeat / slicesPerBeat
 
@@ -50,13 +112,17 @@ object SliceTest {
 
         for (i in 0 until (numberTotalSamples / bufferLengthInSamples).toInt()) {
             correlatedAudioBuffer.add(440.0)
-        }
+        } //gets 86 items
 
-        console.log("Correlated buffer: " + correlatedAudioBuffer)
+        //console.log("Correlated buffer: " + correlatedAudioBuffer)
+
+        val slicesPerCorrelatedBufferItem = (bufferLengthInSamples / sliceLengthInSamples).toInt()
+
+        console.log("Slices if converting the buffer: " + (slicesPerCorrelatedBufferItem * correlatedAudioBuffer.count()))
 
         //take the correlated buffer and convert it to slices
 
-        
+
     }
 
 }
