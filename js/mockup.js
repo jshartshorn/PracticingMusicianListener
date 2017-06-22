d34bcf049c02cb80f0de596be71f1d7129080c0d
John Nastos <jn@Johns-MacBook-Pro.local>
2017-06-22 08:16:41 -0700

    Creating VexFlowUtil

--- js/mockup.js
+++ js/mockup.js
@@ -1,21 +1,17 @@
 
-var canvasName = "notationCanvas";
-
 VF = Vex.Flow;
 
-// Create an SVG renderer and attach it to the DIV element named "boo".
-var div = document.getElementById(canvasName)
-var renderer = new VF.Renderer(div, VF.Renderer.Backends.CANVAS);
+
+var canvasName = "notationCanvas";
+
+var context = VexFlowUtil.setupCanvas(document.getElementById(canvasName));
 
 // Configure the rendering context.
-renderer.resize(600, 500);
-var context = renderer.getContext();
-context.setFont("Arial", 10, "").setBackgroundFillStyle("#ccc");
+//renderer.resize(600, 500);
 
+var staveWidth = 600;
 
-
-// Create a stave of width 400 at position 10, 40 on the canvas.
-var stave = new VF.Stave(20, 40, 500);
+var stave = VexFlowUtil.createStave(staveWidth);
 
 // Add a clef and time signature.
 stave.addClef("treble").addTimeSignature("4/4");
@@ -23,47 +19,17 @@ stave.addClef("treble").addTimeSignature("4/4");
 // Connect it to the rendering context and draw!
 stave.setContext(context).draw();
 
+
+
+
 var exercise = getExerciseNotes(); //pulls from the loaded js file
-var notes = exercise.notes;
-var beats = exercise.beats;
 
-// Create a voice in 4/4 and add above notes
-var voice = new VF.Voice({num_beats: beats,  beat_value: 4});
-voice.addTickables(notes);
-
-
-// Format and justify the notes to 400 pixels.
-var formatter = new VF.Formatter().joinVoices([voice]).format([voice], 450);
-
-// Render voice
-voice.draw(context, stave);
+var voice = VexFlowUtil.inputExercise(context, stave, exercise);
 
 //animation?
-var xToAnimateTo = voice.tickables[4].getAbsoluteX()
 
-var topY = stave.getYForLine(0)
-var bottomY = stave.getYForLine(4)
-//var bottomY = stave.getBoundingBox().y + stave.height
+var indicatorItem = voice.tickables[6]
 
-function drawShape(){
-  // get the canvas element using the DOM
-  var canvas = document.getElementById(canvasName);
+var canvas = document.getElementById(canvasName);
 
-  // Make sure we don't execute when canvas isn't supported
-  if (canvas.getContext) {
-
-	   // use getContext to use the canvas for drawing
-	   var ctx = canvas.getContext('2d');
-
-	   // Stroked triangle
-	   ctx.beginPath();
-	   ctx.moveTo(xToAnimateTo,bottomY);
-	   ctx.lineTo(xToAnimateTo,topY);
-	   ctx.lineTo(45,bottomY);
-	   ctx.closePath();
-	   ctx.stroke();
-
-  }
-}
-
-drawShape()
+VexFlowUtil.drawIndicatorLine(canvas, stave, indicatorItem)
---
+++ js/vexflow_util.js
@@ -0,0 +1,66 @@
+
+VF = Vex.Flow;
+
+var VexFlowUtil = {
+
+    setupCanvas : function(canvas) {
+        var renderer = new VF.Renderer(canvas, VF.Renderer.Backends.CANVAS);
+
+        var context = renderer.getContext();
+        context.setFont("Arial", 10, "").setBackgroundFillStyle("#ccc");
+
+        return context;
+    },
+
+    createStave : function(staveWidth) {
+        var stave = new VF.Stave(20, 40, staveWidth);
+        return stave;
+    },
+
+    inputExercise : function(context, stave, exercise) {
+        var notes = exercise.notes;
+        var beats = exercise.beats;
+
+        // Create a voice in 4/4 and add above notes
+        var voice = new VF.Voice({num_beats: beats,  beat_value: 4});
+        voice.addTickables(notes);
+
+        // Format and justify the notes to 400 pixels.
+        var formatter = new VF.Formatter().joinVoices([voice]).format([voice], stave.end_x - VexFlowUtil.getNotationStartX(stave));
+
+        // Render voice
+        voice.draw(context, stave);
+
+        return voice;
+    },
+
+    getNotationStartX: function(stave) {
+        return stave.start_x;
+    },
+
+    drawIndicatorLine: function(canvas, stave, indicatorItem) {
+        var indicatorPosition = indicatorItem.getAbsoluteX() + indicatorItem.getBoundingBox().w / 2.0
+
+        var indicatorOverflow = 20
+
+        var topY = stave.getYForLine(0) - indicatorOverflow
+        var bottomY = stave.getYForLine(4) + indicatorOverflow
+
+        if (canvas.getContext) {
+
+        	   // use getContext to use the canvas for drawing
+        	   var ctx = canvas.getContext('2d');
+
+               ctx.strokeStyle = '#ff0000';
+
+        	   // Stroked triangle
+        	   ctx.beginPath();
+        	   ctx.moveTo(indicatorPosition,bottomY);
+        	   ctx.lineTo(indicatorPosition,topY);
+        	   ctx.closePath();
+        	   ctx.stroke();
+
+          }
+    }
+
+}
