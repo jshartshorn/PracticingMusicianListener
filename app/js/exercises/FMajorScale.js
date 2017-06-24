681bc398a0efc7fe250db8eafb26f733a4e06a51
John Nastos <jn@Johns-MacBook-Pro.local>
2017-06-23 17:19:32 -0700

    Changed to F Major scale

--- app/js/exercises/FMajorScale.js
+++ app/js/exercises/FMajorScale.js
@@ -3,14 +3,15 @@ var generatedExercise = null
 function generateKotlinExercise() {
     var exercise = new PracticingMusician.com.practicingmusician.exercises.ExerciseDefinition()
     exercise.tempo = 110.0;
-    exercise.notationItems.add_11rb$(new PracticingMusician.com.practicingmusician.notes.Note(65, 1.0, 'f/4'));
-    exercise.notationItems.add_11rb$(new PracticingMusician.com.practicingmusician.notes.Note(67, 1.0, 'g/4'));
-    exercise.notationItems.add_11rb$(new PracticingMusician.com.practicingmusician.notes.Note(69, 1.0, 'a/4'));
-    exercise.notationItems.add_11rb$(new PracticingMusician.com.practicingmusician.notes.Note(70, 1.0, 'Bb/4'));
+    exercise.notationItems.add_11rb$(new PracticingMusician.com.practicingmusician.notes.Note(65, 1.0, 'F4/q'));
+    exercise.notationItems.add_11rb$(new PracticingMusician.com.practicingmusician.notes.Note(67, 1.0, 'G4/q'));
+    exercise.notationItems.add_11rb$(new PracticingMusician.com.practicingmusician.notes.Note(69, 1.0, 'A4/q'));
+    exercise.notationItems.add_11rb$(new PracticingMusician.com.practicingmusician.notes.Note(70, 1.0, 'Bb4/q'));
     exercise.notationItems.add_11rb$(new PracticingMusician.com.practicingmusician.notes.Barline());
-    exercise.notationItems.add_11rb$(new PracticingMusician.com.practicingmusician.notes.Note(72, 1.0, 'c/5'));
-    exercise.notationItems.add_11rb$(new PracticingMusician.com.practicingmusician.notes.Note(74, 1.0, 'd/5'));
-    exercise.notationItems.add_11rb$(new PracticingMusician.com.practicingmusician.notes.Note(76, 1.0, 'e/5'));
-    exercise.notationItems.add_11rb$(new PracticingMusician.com.practicingmusician.notes.Note(77, 1.0, 'f/5'));
+    exercise.notationItems.add_11rb$(new PracticingMusician.com.practicingmusician.notes.Note(72, 1.0, 'C5/q'));
+    exercise.notationItems.add_11rb$(new PracticingMusician.com.practicingmusician.notes.Note(74, 1.0, 'D5/q'));
+    exercise.notationItems.add_11rb$(new PracticingMusician.com.practicingmusician.notes.Note(76, 1.0, 'E5/q'));
+    exercise.notationItems.add_11rb$(new PracticingMusician.com.practicingmusician.notes.Note(77, 1.0, 'F5/q'));
+    exercise.notationItems.add_11rb$(new PracticingMusician.com.practicingmusician.notes.Barline());
     return exercise
 }
