bc8d7cf4b2dfaed66781af172321448a609daa5b
John Nastos <jn@Johns-MacBook-Pro.local>
2017-06-27 16:05:32 -0700

    Fixing logger

--- app/js/logger.js
+++ app/js/logger.js
@@ -2,7 +2,8 @@ var LOG_PRIORITY = 10
 
 //logger
 function pm_log(message, priority) {
-    if (priority >= LOG_PRIORITY) {
+    if (priority == undefined)
+        priority = 0
+    if (priority >= LOG_PRIORITY)
         console.log(message)
-    }
 }
