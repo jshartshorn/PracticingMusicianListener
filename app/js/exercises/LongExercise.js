782b10f9877fd8211a32dbba36895ec71d3749c3
John Nastos <jn@Johns-MacBook-Pro.local>
2017-06-27 09:19:30 -0700

    Adding long exercise

---
+++ app/js/exercises/LongExercise.js
@@ -0,0 +1,74 @@
+function generateExerciseForKotlin() {
+    return {
+        tempo: 110,
+        notes: [
+            {noteNumber: 60, duration: 1.0},
+            {noteNumber: 62, duration: 1.0},
+            {noteNumber: 64, duration: 1.0},
+            {noteNumber: 65, duration: 1.0},
+            {noteNumber: 67, duration: 1.0},
+            {noteNumber: 69, duration: 1.0},
+            {noteNumber: 71, duration: 1.0},
+            {noteNumber: 72, duration: 1.0},
+
+            {noteNumber: 60, duration: 1.0},
+                        {noteNumber: 62, duration: 1.0},
+                        {noteNumber: 64, duration: 1.0},
+                        {noteNumber: 65, duration: 1.0},
+                        {noteNumber: 67, duration: 1.0},
+                        {noteNumber: 69, duration: 1.0},
+                        {noteNumber: 71, duration: 1.0},
+                        {noteNumber: 72, duration: 1.0},
+
+            {noteNumber: 60, duration: 1.0},
+                        {noteNumber: 62, duration: 1.0},
+                        {noteNumber: 64, duration: 1.0},
+                        {noteNumber: 65, duration: 1.0},
+                        {noteNumber: 67, duration: 1.0},
+                        {noteNumber: 69, duration: 1.0},
+                        {noteNumber: 71, duration: 1.0},
+                        {noteNumber: 72, duration: 1.0},
+
+            {noteNumber: 60, duration: 1.0},
+                        {noteNumber: 62, duration: 1.0},
+                        {noteNumber: 64, duration: 1.0},
+                        {noteNumber: 65, duration: 1.0},
+                        {noteNumber: 67, duration: 1.0},
+                        {noteNumber: 69, duration: 1.0},
+                        {noteNumber: 71, duration: 1.0},
+                        {noteNumber: 72, duration: 1.0},
+        ]
+    }
+}
+
+function generateExerciseEasyScoreCode() {
+    return {
+        bars: [
+            {
+                extra_attributes:[{name:'time_signature',value:'4/4'},{name:'clef',value:'treble'},{name:'key_signature',value:"C"}],
+                groups:[{notes:['C4/q','D4/q','E4/q','F4/q']}]
+            },
+            {
+                            groups:[{notes:['G4/q','A4/q','B4/q','C5/q']}]
+                        },
+            {
+                                        groups:[{notes:['C4/q','D4/q','E4/q','F4/q']}]
+                                    },
+            {
+                                        groups:[{notes:['G4/q','A4/q','B4/q','C5/q']}]
+                                    },
+            {
+                                        groups:[{notes:['C4/q','D4/q','E4/q','F4/q']}]
+                                    },
+            {
+                                        groups:[{notes:['G4/q','A4/q','B4/q','C5/q']}]
+                                    },
+            {
+                                        groups:[{notes:['C4/q','D4/q','E4/q','F4/q']}]
+                                    },
+            {
+                                        groups:[{notes:['G4/q','A4/q','B4/q','C5/q']}]
+                                    },
+            ]
+        }
+}
