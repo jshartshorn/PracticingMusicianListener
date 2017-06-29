5e7f2ff1ae0698f9dd6f2978e96f24eff0f7e38c
Doug Puchalski <doug316@gmail.com>
2017-06-29 10:03:31 -0700

    Remove builds from source control

--- gulp-build/js/logger.js
+++
@@ -1,1 +0,0 @@
-function pm_log(o,I){void 0==I&&(I=0),I>=LOG_PRIORITY&&console.log(o)}var LOG_PRIORITY=10;
--- webpack-build/js/logger.js
+++
@@ -1,9 +0,0 @@
-var LOG_PRIORITY = 10
-
-//logger
-function pm_log(message, priority) {
-    if (priority == undefined)
-        priority = 0
-    if (priority >= LOG_PRIORITY)
-        console.log(message)
-}
