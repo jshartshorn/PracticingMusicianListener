2bc17794394cfd2c75861a14a051a83e45a03668
Doug Puchalski <doug316@gmail.com>
2017-07-11 11:50:16 -0700

    Use SASS to allow for variable image path

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,72 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var exec = require('child_process').exec;
+var sass = require('gulp-sass');
+
+gulp.on('stop', function() {  process.exit(0); });
+gulp.on('err', function() { process.exit(1); });
 
 gulp.task('compile_kotlin', [], function () {
-  exec('kotlinc-js src -output tmp/PracticingMusician.js', function (err, stdout, stderr) {
+  return exec('kotlinc-js src -output tmp/PracticingMusician.js', function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
   });
-})
+});
 
-gulp.task('copy_js_files', function() {
-	gulp.src('./app/pm-listener/js/**/*.js')
-	.pipe(uglify())
-	.pipe(gulp.dest('dist/pm-listener/js'))
-})
+gulp.task('copy_js_files', function () {
+  return gulp.src('./app/pm-listener/js/**/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/pm-listener/js/'));
+});
 
-gulp.task('copy_kotlin_compiled_code',function() {
-	gulp.src('tmp/PracticingMusician.js')
-	.pipe(uglify())
-	.pipe(gulp.dest('dist/pm-listener/js'))
-})
+gulp.task('copy_kotlin_compiled_code', function () {
+  return gulp.src('tmp/PracticingMusician.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/pm-listener/js/'));
+});
 
-gulp.task('copy_kotlin',function() {
-	gulp.src('./node_modules/kotlin/kotlin.js')
-	.pipe(uglify())
-	.pipe(gulp.dest('dist/pm-listener/js'))
-})
+gulp.task('copy_kotlin', function () {
+  return gulp.src('./node_modules/kotlin/kotlin.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/pm-listener/js/'));
+});
 
-gulp.task('copy_html', function() {
-	gulp.src('app/app.html')
-	.pipe(gulp.dest('dist/'))
-})
+gulp.task('copy_html', function () {
+  return gulp.src('app/app.html')
+    .pipe(gulp.dest('dist/'));
+});
 
-gulp.task('copy_css', function() {
-	gulp.src('app/pm-listener/css/*')
-	.pipe(gulp.dest('dist/pm-listener/css'))
-})
+gulp.task('compile_sass', function () {
+  return gulp.src('app/pm-listener/sass/notation.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/pm-listener/css/'));
+});
 
-gulp.task('copy_images', function() {
-	gulp.src('app/pm-listener/images/*')
-	.pipe(gulp.dest('dist/pm-listener/images'))
-})
+gulp.task('copy_sass', function () {
+  return gulp.src('app/pm-listener/sass/notation.scss')
+    .pipe(gulp.dest('dist/pm-listener/sass/'));
+});
 
-gulp.task('copy_audio', function() {
-	gulp.src('app/pm-listener/audio/*')
-	.pipe(gulp.dest('dist/pm-listener/audio'))
-})
+gulp.task('copy_images', function () {
+  return gulp.src('app/pm-listener/images/*')
+    .pipe(gulp.dest('dist/pm-listener/images'));
+});
 
-gulp.task('cleanup', [], function() {})
+gulp.task('copy_audio', function () {
+  return gulp.src('app/pm-listener/audio/*')
+    .pipe(gulp.dest('dist/pm-listener/audio'));
+});
 
-gulp.task('default', ['compile_kotlin','copy_kotlin_compiled_code','copy_js_files','copy_kotlin','copy_css','copy_html','copy_images','copy_audio','cleanup'])
+// gulp.task('cleanup', [], function() {})
+
+gulp.task('default', [
+  'compile_kotlin',
+  'copy_kotlin_compiled_code',
+  'copy_js_files',
+  'copy_kotlin',
+  'compile_sass',
+  'copy_sass',
+  'copy_html',
+  'copy_images',
+  'copy_audio'
+]);
