b5b923bfd3f7401a1e816995ad6b829b705b05fc
John Nastos <jn@Johns-MacBook-Pro.local>
2017-07-11 14:12:56 -0700

    Added pitch 101 exercises

---
+++ app/pm-listener/js/exercises/pitch_101/pitch_exercise_3.js
@@ -0,0 +1,40 @@
+function generateExerciseForKotlin() {
+    return {
+        tempo: 110,
+        notes: [
+            {noteNumber: 69, duration: 2.0},
+            {noteNumber: 67, duration: 1.0},
+            {noteNumber: 69, duration: 1.0},
+            {noteNumber: 67, duration: 4.0},
+            {noteNumber: 69, duration: 1.0},
+            {noteNumber: 67, duration: 1.0},
+            {noteNumber: 69, duration: 2.0},
+            {noteNumber: 67, duration: 4.0},
+        ]
+    }
+}
+
+function generateExerciseEasyScoreCode() {
+    return {
+        title: "Little Ditty 3",
+        author: "Practicing Musician Example Author",
+        time_signature: "4/4",
+        tempo: 110,
+        bars: [
+            {
+                extra_attributes:[{name:'time_signature',value:'4/4'},{name:'clef',value:'treble'},{name:'key_signature',value:"C"}],
+                groups:[{notes:['A4/h,G4/q,A4/q']}]
+            },
+            {
+                            groups:[{notes:['G4/w']}]
+                        },
+            {
+                            groups:[{notes:['A4/q,G4/q,A4/h']}]
+                        },
+            {
+                            groups:[{notes:['G4/w']}]
+                        },
+            ],
+        copyrightInfo: "This arrangement © Copyright 2006 Something Music Limited.<br/> All Rights Reserved. International Copyright Secured."
+        }
+}
